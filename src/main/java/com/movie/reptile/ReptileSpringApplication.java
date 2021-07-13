b05e5a74889717fbaa6215ae33f9fd6efb4a6f73
jiangyupeng <765819328@qq.com>
2021-07-13 10:48:43 +0800

    启动类添加

---
+++ src/main/java/com/movie/reptile/ReptileSpringApplication.java
@@ -0,0 +1,18 @@
+package com.movie.reptile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : 765819328@qq.com
+ * @version :
+ * @date : Created in 2021/7/12 23:03
+ * @description: 启动类 修改
+ * @modified By:
+ */
+@SpringBootApplication
+public class ReptileSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReptileSpringApplication.class);
+    }
+}
